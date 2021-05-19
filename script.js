9e3c5daea5625f4261b661d60a3ebf9e628212dd
GRobin8889 <80229056+GRobin8889@users.noreply.github.com>
2021-05-18 23:31:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ window.onclick = function(event) {
 }
 
 ///////// Automatically Highlight Day of the Week in 'find-dealer-section' ////////////
+let day = new Date();
+let weekday = new Array(7);
+weekday[0] = "sun";
+weekday[1] = "mon";
+weekday[2] = "tue";
+weekday[3] = "wed";
+weekday[4] = "thu";
+weekday[5] = "fri";
+weekday[6] = "sat";
+
+document.getElementsByClassName(weekday[day.getDay()])
+[0].classList.add("current-day");
